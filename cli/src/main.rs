587b17a49b77a2460ca1bbe2b128ed174f5ae495
GoogleFeud <volen.sl666@gmail.com>
2021-12-01 16:59:42 +0200

    Importing files, FSFileHost

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -2,9 +2,8 @@ extern crate clap;
 use clap::{Arg, App};
 use std::path::Path;
 use std::ffi::OsStr;
-use std::fs;
 use std::time::{Instant};
-use lazy::semantic_analyzer::{file_host::VirtualFileHost};
+use lazy::semantic_analyzer::{file_host::{FSFileHost, FileHost}};
 use lazy::errors::builder::ErrorFormatter;
 
 fn get_extention_validity(filename: &str) -> Option<&str> {
@@ -34,20 +33,18 @@ fn main() {
     .get_matches();
 
 if let Some(exe_file) = matches.value_of("run") {
-    if Path::new(exe_file).exists() {
+    if Path::new(&exe_file).exists() {
         if get_extention_validity(&exe_file) == Some("lazy") {
-            let source = fs::read_to_string(&exe_file)
-            .expect("Something went wrong reading the file");
+            let mut files = FSFileHost::new();
             let before = Instant::now();
-            let mut files = VirtualFileHost::new();
-            let module = files.create_virtual("main", source.replace("\r\n", "\n"));
+            let module = files.create(&exe_file);
             if matches.is_present("time") {
                 println!("Parsing took {} nanoseconds", before.elapsed().as_nanos());
             }
             match module {
                 Err(errors) => {
                     for error in errors {
-                        println!("{}", files.format_err(&error, "main").unwrap());
+                        println!("{}", files.format_err(&error, &exe_file).unwrap());
                     }
                 },
                 Ok(_module) => {
